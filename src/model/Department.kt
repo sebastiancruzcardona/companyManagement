cc1b332cee663942419e36eb2afd37b27bae4406
Sophie <sophie.rumu@gmail.com>
2025-02-16 15:39:26 -0500

    Added employees CRUD

--- src/model/Department.kt
+++ src/model/Department.kt
@@ -1,158 +1,102 @@
 package model
 
 class Department(
-    var name: String
+    var name: String,
 ) {
     val employees: MutableList<Employee> = mutableListOf()
+
+    // ---------------- Business Logic ----------------  //
 
     /**
      *  Calculates a department's total payroll
      *
      *  @return Sum of every employee's salary
      */
-    fun getPayroll(): Double {
-        return employees.sumOf { it.salary }
-    }
+    fun getPayroll(): Double = employees.sumOf { it.salary }
 
     /**
-     * Counts the total number of employees with a specific job title.
+     * Counts the total number of employees with a specific job title
      *
-     * Iterates through the employees list and counts how many employees
-     * have a job title that matches the given jobTitle String.
-     *
-     * @param jobTitle The job title to filter employees by.
-     *
-     * @return The total number of employees with the specified job title in the department.
+     * @param jobTitle The job title to filter employees by
+     * @return The total number of employees with the specified job title in the department
      */
-    fun getTotalEmployeesByJobTitle(jobTitle: String): Int {
-        return employees.count { it.jobTitle.name == jobTitle }
-    }
+    fun getTotalEmployeesByJobTitle(jobTitle: String): Int = employees.count { it.jobTitle.name == jobTitle }
+
+    // ---------------- Employees CRUD ----------------  //
 
     /**
-     * Adds an employee to the department if the employee to add is not registered
+     * Adds an employee to the department's employee list if the employee is not under another or the same department
      *
-     * Calls createEmployee()
-     * Handles the IllegalStateException with a try catch block
-     *
-     * @param employee The employee to be added to the department.
-     *
-     * @return Unit
+     * @param employee The employee to be added to the department's employee list
+     * @throws EmployeeAlreadyExistsException If the employee is already registered to a department
      * */
     fun addEmployee(employee: Employee) {
-        try {
-            createEmployee(employee)
-        } catch (e: Exception) {
-            println(e.message)
+        if (Company.getAllEmployees().any { it.id == employee.id }) {
+            throw EmployeeAlreadyExistsException(employee.id)
         }
-    }
-
-    /**
-     * Searches an employee in the department by ID.
-     *
-     * Calls findEmployee()
-     * Handles the NoSuchElementException with a try catch block
-     *
-     * @param employeeId The ID to search for.
-     *
-     * @return The Employee object corresponding to the given ID or null
-     * */
-    fun searchEmployee(employeeId: String): Employee? {
-        try {
-            return findEmployee(employeeId)
-        } catch (e: Exception) {
-            println(e.message)
+        if (Company.getAllEmployees().any { it.email == employee.email }) {
+            throw FieldTakenException("Email", employee.email)
         }
-        return null
-    }
-
-    /**
-     * Removes an employee from the department by ID.
-     *
-     * Calls deleteEmployee()
-     * Handles the IllegalStateException with a try catch block
-     *
-     * @param employeeId The ID to search for.
-     *
-     * @return Unit
-     * */
-    fun removeEmployee(employeeId: String) {
-        try {
-            deleteEmployee(employeeId)
-        } catch (e: IllegalStateException) {
-            println(e.message)
-        }
-    }
-
-    /**
-     * Updates an employee's salary and jobTitle searching by ID.
-     *
-     * Calls searchEmployee()
-     *
-     * @param employeeId The ID to search for.
-     * @param newSalary The new salary to assign
-     * @param newJobTitle Te new jobTitle to asign
-     *
-     * @return Unit
-     * */
-    fun updateEmployee(employeeId: String, newSalary: Double, newJobTitle: JobTitle) {
-        val employee: Employee? = searchEmployee(employeeId)
-        if (employee != null) {
-            employee.salary = newSalary
-            employee.jobTitle = newJobTitle
-        }
-    }
-
-    //SERVICE LAYER LIKE FUNCTIONS
-
-    /**
-     * Adds an employee to the department if the employee to add is not registered.
-     *
-     * Checks whether an employee with the same ID already exists in the department.
-     * If the employee is found, an error message is printed.
-     * If the employee is not found, adds the employee.
-     *
-     * @param employee The employee to be added to the department.
-     *
-     * @return Unit
-     *
-     * @throws IllegalStateException If an employee with the same ID already exists in the department.
-     */
-    private fun createEmployee(employee: Employee) {
-        check(employees.find { it.id == employee.id } == null) {
-            "There is already an employee identified with the id ${employee.id} working on department $name"
+        if (Company.jobTitles.none { it.name == employee.jobTitle.name }) {
+            throw JobTitleNotFoundException(employee.jobTitle.name)
         }
         employees.add(employee)
     }
 
     /**
-     * Searches for an employee in the department by their ID.
+     * Searches an employee in the employees list by ID
      *
-     * Looks for an employee in the employees list by employeeId.
-     * If the employee is found, it returns an Employee object.
-     * If no employee with the given ID exists, a NoSuchElementException is thrown.
+     * @param employeeId The ID to search for
+     * @throws EmployeeNotFoundException If no employee exists with inputted ID
+     * */
+    fun findEmployee(employeeId: String): Employee =
+        employees.find { it.id == employeeId } ?: throw EmployeeNotFoundException(employeeId)
+
+    /**
+     * Removes an employee from the department's employee list by ID
      *
-     * @param employeeId The ID to search for.
-     * @return The Employee object corresponding to the given ID.
-     * @throws NoSuchElementException If no employee with the specified ID is found in the department.
-     */
-    private fun findEmployee(employeeId: String): Employee {
-        val employee: Employee = employees.find { it.id == employeeId }
-            ?: throw NoSuchElementException("There is not an employee with id $employeeId working on department $name")
-        return employee
+     * @param employeeId The ID to search for
+     * @throws EmployeeNotFoundException If no employee exists with inputted ID
+     * */
+    fun removeEmployee(employeeId: String) {
+        if (!employees.removeIf { it.id == employeeId }) {
+            throw EmployeeNotFoundException(employeeId)
+        }
     }
 
     /**
-     * Deletes an employee from the department based on ID.
+     * Updates an employee's data
      *
-     * This function removes an employee with the given employeeId from the employees list.
-     * If no employee with the given ID exists, it throws an IllegalStateException.
-     *
-     * @param employeeId The ID of the employee to be removed.
-     * @throws IllegalStateException If the employee does not exist in the department.
-     */
-    private fun deleteEmployee(employeeId: String) {
-        check(employees.removeIf { employee -> employee.id == employeeId }) {
-            "Could not remove. There is not an employee with id $employeeId working on department $name."
+     * @param employeeId The employee to update
+     * @param name The new name
+     * @param gender The new gender
+     * @param email The new email
+     * @param salary The new salary
+     * @param jobTitle The new job title
+     * */
+    fun updateEmployee(
+        employeeId: String,
+        name: String? = null,
+        gender: String? = null,
+        email: String? = null,
+        salary: Double? = null,
+        jobTitle: JobTitle? = null,
+    ) {
+        val employee = findEmployee(employeeId)
+
+        if (email != null && Company.getAllEmployees().any { it.email == email && it.id != employeeId }) {
+            throw FieldTakenException("Email", email)
+        }
+        if (jobTitle != null && Company.jobTitles.none { it.name == jobTitle.name }) {
+            throw JobTitleNotFoundException(jobTitle.name)
+        }
+
+        employee.apply {
+            name?.let { this.name = it }
+            gender?.let { this.gender = it }
+            email?.let { this.email = it }
+            salary?.let { this.salary = it }
+            jobTitle?.let { this.jobTitle = it }
         }
     }
 }
